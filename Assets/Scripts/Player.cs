14c259c0228893859351ea091cb382fc9ee174da
Mohammad Sadeghi <moh02sadeghi@gmail.com>
2025-03-20 19:10:25 +0330

    feat: add player model and rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float velocity = 7.5f;
+    [SerializeField] private float rotationSpeed = 10f;
 
     private void Update() {
         // Get input
@@ -12,10 +13,14 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) inputVector.y = 1;
         if (Input.GetKey(KeyCode.S)) inputVector.y = -1;
         inputVector.Normalize();
-        
-        // Move
+
         var movementDirection = new Vector3(inputVector.x, 0, inputVector.y);
+
+        // Move
         var movement = movementDirection * (velocity * Time.deltaTime);
         transform.position += movement;
+
+        // Rotate
+        transform.forward = Vector3.Slerp(transform.forward, movementDirection, rotationSpeed * Time.deltaTime);
     }
 }
