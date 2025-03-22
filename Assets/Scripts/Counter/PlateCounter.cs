6f334f8248dbdffe3df9b759b4e1f7f5b7d95789
Mohammad Sadeghi <moh02sadeghi@gmail.com>
2025-03-22 16:08:28 +0330

    feat: add plate counter

---
+++ Assets/Scripts/Counter/PlateCounter.cs
@@ -0,0 +1,21 @@
+﻿using ScriptableObjects;
+using UnityEngine;
+
+namespace Counter {
+    public class PlateCounter : BaseCounter {
+        [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+        public override void Interact(Player.Player player) {
+            if (!player.HasKitchenObject()) {
+                KitchenObject.KitchenObject.SpawnKitchenObject(
+                    plateKitchenObjectSO,
+                    player
+                );
+            }
+        }
+
+        public override void InteractAlternate() {
+            // Do Nothing
+        }
+    }
+}
